e87df80c94f0f248db1a35acd937127f19b3b5c3
Cleber <cleber_batista2010@hotmail.com>
2021-12-13 00:09:46 -0300

    Configurando Mensagem exception

--- src/main/java/org/github/cleberGraciano/clientes/exception/ApplicationControllerAdvice.java
+++ src/main/java/org/github/cleberGraciano/clientes/exception/ApplicationControllerAdvice.java
@@ -29,7 +29,7 @@ public class ApplicationControllerAdvice {
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity handlerResponseException(ResponseStatusException ex) {
-        String mensagemErro = ex.getMessage();
+        String mensagemErro = ex.getReason();
         HttpStatus codigoStatus = ex.getStatus();
         ApiErrors apiErrors = new ApiErrors(mensagemErro);
         return new ResponseEntity(apiErrors, codigoStatus);
